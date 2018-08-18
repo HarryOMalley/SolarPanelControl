e59f1b932e355eb4b045b8d3a988d16ef7e85443
Harry O'Malley <hazaomalley@gmail.com>
2018-08-18 16:24:18 +0100

    Initial commit, testing a relay

---
+++ RelayTest/RelayTest/RelayTest.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		RelayTest.ino
+ Created:	8/18/2018 4:24:09 PM
+ Author:	Harry
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
